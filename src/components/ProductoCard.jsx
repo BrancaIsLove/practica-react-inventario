cdb06b35391bc950df51930925c5f9a0a31c56bb
Jimenez Facundo <fjimenez@ubp.edu.ar>
2026-07-30 06:15:13 -0300

    cambio de estilos y cartel de stock

--- src/components/ProductoCard.jsx
+++ src/components/ProductoCard.jsx
@@ -3,7 +3,9 @@ function ProductCard({nombre, precio, stock}){
         <div className="card">
             <h3 className="card-name">{nombre}</h3>
             <p className="card-price">Precio: ${precio}</p>
-            <p className=""card-stock>Stock: {stock}</p>
+            <p className="card-stock">Stock: {stock}</p>
+            {stock < 10 && stock >= 1 &&<span className="badge">Stock bajo</span>}
+            {stock === 0 && <span className="sin-stock">Sin stock</span>}
         </div>
     )
 }
--- src/data/products.js
+++ src/data/products.js
@@ -3,5 +3,5 @@ export const products=[
     {id:2, name: "Resma A4", price:12000, stock:8},
     {id:3, name: "Fotocopia", price:100, stock:999},
     {id:4, name: "Cuaderno rayado", price:3500, stock:22},
-    {id:5, name: "Birome azul", price:800, stock:60}
+    {id:5, name: "Birome azul", price:800, stock:0}
 ]
